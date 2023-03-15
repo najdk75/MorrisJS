271e16f018f9253baed0de9a7b33d2402fd193e6
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-15 11:21:30 +0100

    test

--- test.php
+++ test.php
@@ -3,18 +3,17 @@
 
         //1° - Connexion à la BDD
         $base = new PDO('mysql:host=localhost; dbname=id20205717_coronavirus', 'id20205717_najd_kacem', 'u0/?#Y7S%37Cy\=i');
+        $base->exec("SET CHARACTER SET utf8");
 
         //2° - Prépareation de requette et execution
-        $sql = "SELECT Continent, SUM(TotalCases) as total_cas_par_continent
-        FROM Covid
-        GROUP BY Continent";
+        $sql = "SELECT * FROM Covid";
 
         $result = $base->query($sql)
 
         if ($result->num_rows > 0) {
             // Parcourir les résultats et afficher chaque tuple (continent, nombre de cas)
             while($row = $result->fetch_assoc()) {
-                echo "Continent: " . $row["continent"]. " - Nombre de cas: " . $row["total_cas_par_continent"]. "<br>";
+                echo "Continent: " . $row["Continent"] . "<br>";
             }
         } else {
             echo "0 résultats";
