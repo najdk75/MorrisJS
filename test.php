cb42487afe39c887d7a873d665e70920910e26e7
najdk75 <107084044+najdk75@users.noreply.github.com>
2023-03-15 11:34:24 +0100

    test

--- test.php
+++ test.php
@@ -2,18 +2,18 @@
         //Lister le coeeentenu de la table movies
 
         //1° - Connexion à la BDD
-        $base = new PDO('mysql:host=localhost; dbname=id20205717_coronavirus', 'id20205717_najd_kacem', 'u0/?#Y7S%37Cy\=i');
+        $base = new PDO('mysql:host=localhost; dbname=id20205717_coronavirus2', 'id20205717_najd', '|Jwa]c44ek(]A0cl');
         $base->exec("SET CHARACTER SET utf8");
 
         //2° - Prépareation de requette et execution
-        $sql = "SELECT * FROM Covid";
+        $sql = "SELECT * FROM pays";
 
         $result = $base->query($sql)
 
         if ($result->num_rows > 0) {
             // Parcourir les résultats et afficher chaque tuple (continent, nombre de cas)
             while($row = $result->fetch_assoc()) {
-                echo "Continent: " . $row['Continent'] . "<br>";
+                echo "Pays: " . $row['nom'] ."nombre de cas : ". $row['total_cas'] . "<br>";
             }
         } else {
             echo "0 résultats";
